d904c06de7dbce67ecad25241ef837c797e15d78
Rebecca Allen <rebeccazarsky@gmail.com>
2016-10-16 15:41:06 -0400

    add more logic in to js files

--- js/gettr-interface.js
+++ js/gettr-interface.js
@@ -3,7 +3,7 @@ var Gettr = require('./../js/gettr.js').gettrModule;
 $(document).ready(function(){
   var currentGettr = new Gettr();
   $('#submit').click(function(){
-    var name = $('#name').val();
+    var identity = $('#name').val();
     currentGettr.getRepos(name);
   });
 });
--- js/gettr.js
+++ js/gettr.js
@@ -1,15 +1,30 @@
 var apiKey = require('./../.env').apiKey;
 
 Gettr = function(){
-
 }
 
-Gettr.prototype.getRepos = function(name){
-  $.get('https://api.github.com/users/' + name + '?access_token=' + apiKey).then(function(response){
+Gettr.prototype.getRepos = function(identity){
+  $.get('https://api.github.com/users/' + identity + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('.selectedGettr').text(response.login);
+    $('.gettrAddress').attr(src, response.html_url);
+    $('.gettrName').text(response.name);
+    $('.gettrEmail').text(response.email);
   }).fail(function(error){
     console.log(error.responseJSON.message);
+    $('.selectedGettr').text('Gettr Unsucessful, try again yo!');
   });
+}
+
+$.get('https://api.github.com/users/' + identity + '/repos?access_token=' + apiKey).then(function(response){
+  console.log(response);
+  $('.selectedGettrRepos').text('');
+  for(i=0; i<response.length; i++){
+    $('.selectedGettrRepos'),append('<li>' + response[i].name + "<ul><li>" + response[i].created_at + '</li><li><a href="' + response[i].url + '"></a></li><li>' + response[i].description + '</li>')
+  }
+}).fail(function(error){
+  console.log(error.responseJSON.message);
+});
 }
 
 exports.gettrModule = Gettr;
