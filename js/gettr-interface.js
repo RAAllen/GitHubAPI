8b5ac200a6280f4a2fe1e5b2b05711ad275aee9b
Rebecca Allen <rebeccazarsky@gmail.com>
2016-10-16 18:17:17 -0400

    small bug fixes and styling

--- js/gettr-interface.js
+++ js/gettr-interface.js
@@ -3,7 +3,14 @@ var Gettr = require('./../js/gettr.js').gettrModule;
 $(document).ready(function(){
   var currentGettr = new Gettr();
   $('#submit').click(function(){
+    $('#display-user').toggle();
+    $('#search').toggle();
     var identity = $('#name').val();
+    $('#name').val('');
     currentGettr.getRepos(name);
+  });
+  $('#toggle-back').click(function(){
+    $('#display-user').toggle();
+    $('#search').toggle();
   });
 });
--- js/gettr.js
+++ js/gettr.js
@@ -14,23 +14,18 @@ Gettr.prototype.getRepos = function(identity){
     console.log(error.responseJSON.message);
     $('.selectedGettr').text('Gettr Unsucessful, try again yo!');
   });
-}
-
-$.get('https://api.github.com/users/' + identity + '/repos?access_token=' + apiKey).then(function(response){
-  console.log(response);
-  $('.selectedGettrRepos').text('');
-  for(i=0; i<response.length; i++){
-    $('.selectedGettrRepo').append('<td>' + response[i].name + '</td><td>' + response[i].created_at + '</td><td>' + response[i].url + '</td><td>' + response[i].description + </td>);
-  }
-}).fail(function(error){
-  console.log(error.responseJSON.message);
-});
+  $.get('https://api.github.com/users/' + identity + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    $('.selectedGettrRepos').text('');
+    for(i=0; i<response.length; i++){
+      $('.selectedGettrRepo').append('<td>' + response[i].name + '</td><td>' + response[i].created_at + '</td><td>' + response[i].url + '</td><td>' + response[i].description + '</td>');
+    }
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
 }
 
 exports.gettrModule = Gettr;
-
-
-
 
 // {
 //   $('.selectedGettrRepos').append('<li>' + response[i].name + "<ul><li>" + response[i].created_at + '</li><li><a href="' + response[i].url + '"></a></li><li>' + response[i].description + '</li>')
